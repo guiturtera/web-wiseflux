ce21c568d4c7964bd0a751053652406d4c488df6
Guilherme Turtera <guiturtera@hotmail.com>
2023-11-18 21:03:14 -0300

    fix: variable name

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = 'https://localhost:7010/api/v1'//process.env.apiUrl + "/api/v1";
-const baseURL = process.env.VUE_APP_API_URL + "/api/v1";
+//const baseURL = process.env.VUE_APP_API_URL + "/api/v1";S
+const baseURL = import.meta.env.VITE_APP_API_URL + "/api/v1";
 
 const instance = axios.create({
   baseURL: baseURL,
