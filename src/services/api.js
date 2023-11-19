499af24c0573455b220e303572310aec9e82bd48
Guilherme Turtera <guiturtera@hotmail.com>
2023-11-18 21:13:46 -0300

    ci: fix ci cd bug

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 console.log(import.meta.env.VITE_APP_API_URL)
 //const baseURL = process.env.VUE_APP_API_URL + "/api/v1";S
-const baseURL = import.meta.env.VITE_APP_API_URL + "/api/v1";
+const baseURL = 'https://' + import.meta.env.VITE_APP_API_URL + "/api/v1";
 
 const instance = axios.create({
   baseURL: baseURL,
